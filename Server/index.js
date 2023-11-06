5cc97ae1a4bed6a5e52d0bc78b30aa718e1209c5
Natia Lollie <natialollie.developer@gmail.com>
2023-11-06 14:41:56 -0600

    chore: update CRUD operation HTTP methods to include dynamic data for req; test in postman

--- Server/index.js
+++ Server/index.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // define port for server to listen on
 const port = 4000;
@@ -32,32 +37,59 @@ app.listen(port, () => {
     console.log(`I am listening to port:${port}`);
 });
 
-// C R U D (dummy data)
-
-/* 
-let schoolTask = new Task(
-    {
-        name: "book is read",
-        date: "30 Oct 2023",
-        isDone: false,
-    }
-)
-schoolTask.save();
-*/
-
-// Restful API | HTTP Methods: Post (create), Get, Put, Patch, Delete
+// C R U D
+// Restful API | HTTP Methods: Post (create), Get (read), Put (update), Patch (update), Delete
 
 app.post('/task', async (req, res) => {
-    let schoolTask = new Task(
-        {
-            name: "book is read",
-            date: "30 Oct 2023",
-            isDone: false,
-        }
-    )
-    let data = await schoolTask.save();
-    res.send(data);
+    try {
+        let createTask = new Task({
+            name: req.body.name,
+            date: req.body.date,
+            isCompleted: false,
+        });
+        let savedTask = await createTask.save();
+        res.send(savedTask);
+    } catch (err) {
+        console.log(err)
+        res.send('failed to create new task.')
+    }
+});
+
+app.get('/get_task', async (req, res) => {
+    try {
+        let allTasks = await Task.find();
+        res.send(allTasks);
+    } catch (err) {
+        console.log(err);
+        res.send('failed to get tasks')
+    }
 })
+
+app.put('/update_task', async (req, res) => {
+    try {
+        let updatedTask =  await Task.findOneAndUpdate({ _id: req.body.id }, {isCompleted: req.body.isCompleted});
+        res.send(`successfully updated task: ${updatedTask}`);
+    } catch (err) {
+        console.log(err);
+        res.send('failed to update task.');
+    }
+});
+
+app.delete('/delete_task', async (req, res) => {
+    try {
+        await Task.findOneAndDelete({ _id: req.body.id });
+        res.send({message: 'successfully deleted task.'});
+    } catch (err) {
+        console.log(err);
+        res.send('failed to delete task.');
+    }
+})
+
+
+
+
+
+
 
 
 
